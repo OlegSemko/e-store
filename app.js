610569b9da44641d44160db87484baa7b4f78679
Oleg Semko <oleg.semko@zazmic.com>
2021-10-17 19:27:37 +0300

    added home page

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 app.use('/users', userRoute);
 app.use('/products', productRoute);
 app.use('/orders', orderRoute);
+app.get('/', (req, res) => {
+  res.send('home page');
+});
 app.use((req, res, next) => {
   res.status(404).send('Not found');
 });
